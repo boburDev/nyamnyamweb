bca54a82bf71368e931594893ed506f0a22c8876
Abduraxmon-st <2442abd72@gmail.com>
2025-09-03 16:17:01 +0500

    token refreshing  functions added

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,7 @@
 export const ACCESS_TOKEN = "NYAM_ACCESS_TOKEN";
 export const REFRESH_TOKEN = "NYAM_REFRESH_TOKEN";
 export const DOMAIN = process.env.NEXT_PUBLIC_API_URL;
+export const REFRESH_USER = `${DOMAIN}/token/refresh/`;
 export const SIGNIN = `${DOMAIN}/auth/login/`;
 export const SIGNUP = `${DOMAIN}/auth/register/`;
 export const OTP_UPDATE = `${DOMAIN}/otp/verify-update/`;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,3 @@
 export * from "./auth-type";
 export * from "./user-layout";
+export * from "./token-type"
