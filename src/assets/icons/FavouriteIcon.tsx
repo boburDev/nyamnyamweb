c8f610be9d4f94b9f7762f17ea75091bca37ff13
Shuxratxoja <shuxratxujaislomovich@gmail.com>
2025-09-12 17:01:35 +0500

    fixed icons eror

--- src/assets/icons/FavouriteIcon.tsx
+++ src/assets/icons/FavouriteIcon.tsx
@@ -4,7 +4,7 @@ export const FavouriteIcon = (props: React.SVGProps<SVGSVGElement>) => {
       width={16}
       height={21}
       viewBox="0 0 16 21"
-      fill="none"
+      fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
